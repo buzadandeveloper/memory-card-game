2bfecd5072e6437e166967e0979c46347a64236f
Buza Dan <buzadandeveloper@gmail.com>
2024-10-03 09:47:35 +0300

    feat: implement logic for new game

--- src/store/game/reducer.js
+++ src/store/game/reducer.js
@@ -1,9 +1,13 @@
+import * as actions from "./actionsType";
 const gameState = {
+  isNewGame: false,
   turns: [],
 };
 
 const reducer = (state = gameState, action) => {
   switch (action.type) {
+    case actions.NEW_GAME:
+      return { ...state, isNewGame: true };
     default:
       return state;
   }
