617530a67c2a2dc46b6e7e482e2be5d3d49c49be
Buza Dan <buzadandeveloper@gmail.com>
2024-10-03 09:07:28 +0300

    feat: implement fetch logic

--- src/store/cards/reducer.js
+++ src/store/cards/reducer.js
@@ -1,3 +1,5 @@
+import * as actions from "./actionsType";
+
 const cardsState = {
   cards: [],
   flippedCards: [],
@@ -8,6 +10,12 @@ const cardsState = {
 
 const reducer = (state = cardsState, action) => {
   switch (action.type) {
+    case actions.FETCH_CARDS_REQUEST:
+      return { ...state, loading: true, error: null };
+    case actions.FETCH_CARDS_SUCCESS:
+      return { ...state, loading: false, cards: action.payload };
+    case actions.FETCH_CARDS_ERROR:
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
