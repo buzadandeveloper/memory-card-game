830543bc55a19eb9121abfd978d0a57a203ae990
Buza Dan <buzadandeveloper@gmail.com>
2024-10-03 10:10:17 +0300

    feat: implement card flip logic

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,4 +1,7 @@
 import styled from "@emotion/styled";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { flipCard } from "../../store/cards/actions";
 
 const SingleCardContainer = styled.div``;
 
@@ -8,9 +11,34 @@ const ImgaeCard = styled.img`
   object-fit: cover;
 `;
 
-export const SingleCard = ({ card }) => {
+export const SingleCard = ({ card, index }) => {
+  const dispatch = useDispatch();
+  const { flippedCards } = useSelector((state) => state.cards);
+
+  const handleChoiceCard = (card, index) => {
+    if (
+      flippedCards.length < 2 &&
+      !flippedCards.some((flippedCard) => flippedCard.index === index)
+    ) {
+      dispatch(flipCard({ ...card, index }));
+    }
+
+    console.log(index);
+  };
+
+  useEffect(() => {
+    console.log(flippedCards);
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].id === flippedCards[1].id) {
+        console.log("true");
+      } else {
+        console.log("false");
+      }
+    }
+  }, [flippedCards]);
+
   return (
-    <SingleCardContainer>
+    <SingleCardContainer onClick={() => handleChoiceCard(card, index)}>
       <ImgaeCard src={card.image} />
     </SingleCardContainer>
   );
