885622619a5e24f8b1ce1743ce1d403d5eea6450
Buza Dan <buzadandeveloper@gmail.com>
2024-10-05 11:14:51 +0300

    chore: add ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./GlobalStyles/theme";
 import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
 import { GameView } from "./components/GameView/GameView";
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <GameView />
-    </>
+    </ThemeProvider>
   );
 }
 
