7d020fe499ebacd4ac558d7b8a582151f24a3f23
Buza Dan <buzadandeveloper@gmail.com>
2024-10-07 11:14:49 +0300

    style: styling component

--- src/components/NewGameButton/NewGameButton.jsx
+++ src/components/NewGameButton/NewGameButton.jsx
@@ -1,18 +1,30 @@
 import { useDispatch } from "react-redux";
 import { startNewGame } from "../../store/game";
-import { fetchCards } from "../../store/cards";
+import { fetchCards, flipReset } from "../../store/cards";
 import styled from "@emotion/styled";
 
 export const NewGameButton = () => {
   const dispatch = useDispatch();
   const newGame = () => {
+    dispatch(flipReset());
     dispatch(startNewGame());
     setTimeout(() => {
       dispatch(fetchCards());
-    }, 2000);
+    }, 1000);
   };
 
   return <Button onClick={newGame}>New Game</Button>;
 };
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.jet_black};
+  color: ${({ theme }) => theme.colors.light_grey};
+  border: 1px solid white;
+  cursor: pointer;
+  padding: 0.4em;
+  border-radius: 12px;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.light_jet_black};
+    transition: 0.3s ease;
+  }
+`;
