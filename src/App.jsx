63693f242d19948af1aad1df6e81f128538176cc
Buza Dan <buzadandeveloper@gmail.com>
2024-10-07 11:17:11 +0300

    feat: added BarGame

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { ThemeProvider } from "@emotion/react";
 import { theme } from "./GlobalStyles/theme";
 import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
 import { GameView } from "./components/GameView/GameView";
+import { BarGame } from "./components/BarGame/BarGame";
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <BarGame />
       <GameView />
     </ThemeProvider>
   );
