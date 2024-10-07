4e90fd92fca9c58f1e6c4139934ba0702f280e14
Buza Dan <buzadandeveloper@gmail.com>
2024-10-07 11:14:08 +0300

    feat: updated GameView

--- src/components/GameView/GameView.jsx
+++ src/components/GameView/GameView.jsx
@@ -5,13 +5,18 @@ import { SingleCard } from "../SingleCard/SingleCard";
 import { NewGameButton } from "../NewGameButton/NewGameButton";
 import { flipCard, matchCards, flipReset } from "../../store/cards";
 import { incrementTurns, gameWon } from "../../store/game";
+import { fetchCards } from "../../store/cards";
 
 export const GameView = () => {
   const dispatch = useDispatch();
-  const { cards, flippedCards, matchedCards } = useSelector(
+  const { cards, flippedCards, matchedCards, initialLoading } = useSelector(
     (state) => state.cards
   );
-  const { turns, win, bestTurns } = useSelector((state) => state.game);
+  const { turns, win } = useSelector((state) => state.game);
+
+  useEffect(() => {
+    dispatch(fetchCards());
+  }, [dispatch]);
 
   const handleChoiceCard = (card, index) => {
     if (
@@ -43,33 +48,58 @@ export const GameView = () => {
 
   return (
     <GameViewContainer>
+      {initialLoading && <LoadingSpinner className="loader" />}
+      {win && <WonText>You Won !</WonText>}
       <GameViewGrid>
-        {cards.map((card, index) => (
-          <SingleCard
-            key={index}
-            card={card}
-            index={index}
-            handleChoiceCard={handleChoiceCard}
-          />
-        ))}
+        {cards.length > 0 &&
+          cards.map((card, index) => (
+            <SingleCard
+              key={index}
+              card={card}
+              index={index}
+              handleChoiceCard={handleChoiceCard}
+            />
+          ))}
       </GameViewGrid>
-      <NewGameButton />
-      <h1>turns: {turns}</h1>
-      <h1>best turns: {bestTurns}</h1>
-      {win && <h1>You Won</h1>}
+      <GameViewFooter>
+        <TurnsText>Turns: {turns}</TurnsText>
+        <NewGameButton />
+      </GameViewFooter>
     </GameViewContainer>
   );
 };
 
 const GameViewContainer = styled.div`
-  height: 100vh;
+  height: 91vh;
   width: 100%;
-  background: rgb(28, 29, 33);
+  background: ${({ theme }) => theme.colors.jet_black};
   background: radial-gradient(circle, #2e313c 0%, #2b2929 100%);
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  position: relative;
+`;
+
+const WonText = styled.h1`
+  color: ${({ theme }) => theme.colors.jet_black};
+  text-shadow: 1px 1px 0 white, -1px 1px 0 white, 1px -1px 0 white,
+    -1px -1px 0 white;
+  font-weight: 500;
+  position: absolute;
+  z-index: 100;
+  top: 50%;
+  left: 50%;
+  transition: 0.7s ease-in;
+  animation: slideIn 1s ease forwards;
+  @keyframes slideIn {
+    0% {
+      transform: translate(-50%, -300%);
+    }
+    100% {
+      transform: translate(-50%, -230%);
+    }
+  }
 `;
 
 const GameViewGrid = styled.div`
@@ -78,4 +108,42 @@ const GameViewGrid = styled.div`
   grid-template-columns: repeat(4, 100px);
   gap: 1em;
   margin-bottom: 4em;
+  transition: 0.6s ease-in;
+`;
+
+const TurnsText = styled.p`
+  color: ${({ theme }) => theme.colors.light_grey};
+  margin-top: 3em;
+  margin-bottom: 1em;
+`;
+
+const GameViewFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoadingSpinner = styled.div`
+  position: absolute;
+  top: 38%;
+  left: 48%;
+  transform: translate(-50%, -50%);
+  &.loader {
+    width: 48px;
+    height: 48px;
+    border: 5px solid #fff;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
