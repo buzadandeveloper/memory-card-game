f0fa6679700af2e957abb2990ef138801c64adb2
Buza Dan <buzadandeveloper@gmail.com>
2024-10-09 18:23:04 +0300

    feat: updated shuffle

--- src/utils/shuffleCards.js
+++ src/utils/shuffleCards.js
@@ -1,4 +1,8 @@
-export const shuffleCards = (cards) => {
-  const deck = [...cards, ...cards].sort(() => Math.random() - 0.5);
+export const shuffleCards = (cards, selectedCardValue) => {
+  let deck = [];
+  for (let i = 0; i < selectedCardValue; i++) {
+    deck = [...deck, ...cards];
+  }
+  deck.sort(() => Math.random() - 0.5);
   return deck;
 };
