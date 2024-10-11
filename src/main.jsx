fcfd182ef0346a14cbcd5dcbaec3563157aaf74a
Buza Dan <buzadandeveloper@gmail.com>
2024-10-11 12:23:29 +0300

    fix: corrected review comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
